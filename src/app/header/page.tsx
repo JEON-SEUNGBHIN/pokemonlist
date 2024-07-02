032f665586f78df7945b67acf64b9149ab7ceabb
JEON SEUNGBHIN <tmdqls104@naver.com>
2024-07-02 22:55:24 +0900

    update:  header 생성

---
+++ src/app/header/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const HeaderPage = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/">포켓몬 도감</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default HeaderPage;
